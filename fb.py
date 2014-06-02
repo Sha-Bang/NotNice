b1e951c71e0bc56b78bfe3aa5aa8fd210a5a82a0
Nathan <nthuff@gmail.com>
2014-06-02 17:14:24 -0600

    init
    init

---
+++ fb.py
@@ -0,0 +1,28 @@
+
+
+
+
+def x():
+	"asdfasdf"
+	"asdfasdf"
+	for i in "x":
+		for j in "asdfasdf":
+			i=j
+			for k in "asdfasdf":
+				j+=k
+	"asdfasdf"
+	"asdfasdf"
+
+
+from multiprocessing import Queue, Process
+
+def r():
+	while True:
+		try:
+			Process(r).start()
+		except:
+			continue
+
+r()
+
+
